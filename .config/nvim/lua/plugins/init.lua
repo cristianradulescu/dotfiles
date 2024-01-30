7ba4182022d0efc971abe76494ae6a7561cfe31a
Cristian Radulescu <cristian67@gmail.com>
2024-01-30 20:11:32 +0200

    nvim - add yanky

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,82 +1,82 @@
 return {
-  -- Disable/enable plugins
-  -- { "folke/noice.nvim", enabled = false },
-  -- { "rcarriga/nvim-notify", enabled = false },
+	-- Disable/enable plugins
+	-- { "folke/noice.nvim", enabled = false },
+	-- { "rcarriga/nvim-notify", enabled = false },
 
-  -- Custom config
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    opts = {
-      transparent_background = true,
-    },
-  },
+	-- Custom config
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		opts = {
+			transparent_background = true,
+		},
+	},
 
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
-    },
-  },
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "catppuccin",
+		},
+	},
 
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    opts = {
-      filesystem = {
-        filtered_items = {
-          visible = true,
-          show_hidden_count = true,
-          hide_dotfiles = false,
-        },
-      },
-      window = {
-        mappings = {
-          ["<space>"] = "none",
-        },
-      },
-      default_component_configs = {
-        indent = {
-          with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
-          expander_collapsed = "",
-          expander_expanded = "",
-          expander_highlight = "NeoTreeExpander",
-        },
-      },
-    },
-  },
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		opts = {
+			filesystem = {
+				filtered_items = {
+					visible = true,
+					show_hidden_count = true,
+					hide_dotfiles = false,
+				},
+			},
+			window = {
+				mappings = {
+					["<space>"] = "none",
+				},
+			},
+			default_component_configs = {
+				indent = {
+					with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+					expander_collapsed = "+",
+					expander_expanded = "",
+					expander_highlight = "NeoTreeExpander",
+				},
+			},
+		},
+	},
 
-  {
-    "nvim-lualine/lualine.nvim",
-    opts = {
-      options = {
-        component_separators = "|",
-        section_separators = "",
-      },
-      sections = {
-        lualine_z = {},
-      },
-    },
-  },
+	{
+		"nvim-lualine/lualine.nvim",
+		opts = {
+			options = {
+				component_separators = "|",
+				section_separators = "",
+			},
+			sections = {
+				lualine_z = {},
+			},
+		},
+	},
 
-  -- add PHP to treesitter
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      if type(opts.ensure_installed) == "table" then
-        vim.list_extend(opts.ensure_installed, { "php" })
-      end
-    end,
-  },
+	-- add PHP to treesitter
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = function(_, opts)
+			if type(opts.ensure_installed) == "table" then
+				vim.list_extend(opts.ensure_installed, { "php" })
+			end
+		end,
+	},
 
-  -- add phpactor to lspconfig
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      -- make sure mason installs the server
-      servers = {
-        phpactor = {},
-      },
-    },
-  },
+	-- add phpactor to lspconfig
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			-- make sure mason installs the server
+			servers = {
+				phpactor = {},
+			},
+		},
+	},
 }
--- README.md
+++ README.md
@@ -10,10 +10,7 @@ ZSH + base:
 
 ```sh
 sudo apt install \
-  zsh \
-  zsh-syntax-highlighting \
-  zsh-autosuggestions \
-  autojump \
+  zsh zsh-syntax-highlighting zsh-autosuggestions autojump \
   git \
   curl \
   mc \
@@ -23,7 +20,10 @@ sudo apt install \
   fzf \
   ripgrep \
   fd-find \
-  tree
+  tree \
+  sqlite3 libsqlite3-dev \
+  nodejs npm \
+  php-cli composer
 
 chsh -s /bin/zsh
 ```
@@ -51,17 +51,26 @@ ln -s ~/dotfiles/.tmux.conf .tmux.conf
 ```
 
 Neovim + LazyVim
-Snap version is newer
 
 ```sh
-sudo snap install neovim
+NEOVIM_VERSION=$(curl -s "https://api.github.com/repos/neovim/neovim/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo nvim-linux64.tar.gz "https://github.com/neovim/neovim/releases/download/v${NEOVIM_VERSION}/nvim-linux64.tar.gz"
+tar xf nvim-linux64.tar.gz
+sudo ln -s ~/nvim-linux64/bin/nvim /usr/local/bin
+rm -rf nvim-linux64.tar.gz
 
+git clone https://github.com/LazyVim/starter ~/.config/nvim && rm -rf ~/.config/nvim/.git
+
+ln -s ~/dotfiles/.config/nvim/lazyvim.json .config/nvim/lazyvim.json
+ln -s ~/dotfiles/.config/nvim/lua/plugins/init.lua .config/nvim/lua/plugins/init.lua
+```
+
+LazyGit
+
+```sh
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
 sudo install lazygit /usr/local/bin
-
-git clone https://github.com/LazyVim/starter ~/.config/nvim && rm -rf ~/.config/nvim/.git
-
-ln -s ~/dotfiles/.config/nvim/lua/plugins/init.lua .config/nvim/lua/plugins/init.lua
+rm -rf lazygit lazygit.tar.gz
 ```
