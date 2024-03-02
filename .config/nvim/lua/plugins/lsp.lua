5694fba49ef982571bf4364b71adb7ad023a8f1b
Cristian Radulescu <cristian67@gmail.com>
2024-03-03 01:19:48 +0200

    nvim - lsp config updates

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -6,12 +6,12 @@ return {
     event = "BufReadPost",
     dependencies = {
       -- Automatically install LSPs to stdpath for neovim
-      { "williamboman/mason.nvim",          config = true },
+      { "williamboman/mason.nvim", config = true },
       { "williamboman/mason-lspconfig.nvim" },
 
       -- Useful status updates for LSP
-      { "j-hui/fidget.nvim",                tag = "legacy", opts = {} },
-      -- { "folke/neoconf.nvim", cmd = "Neoconf", config = false, dependencies = { "nvim-lspconfig" } },
+      { "j-hui/fidget.nvim", tag = "legacy", opts = {} },
+      
       -- Additional lua configuration, makes nvim stuff amazing!
       "folke/neodev.nvim",
 
@@ -25,8 +25,16 @@ return {
           "hrsh7th/cmp-nvim-lsp-signature-help",
           "hrsh7th/cmp-path",
           "hrsh7th/cmp-nvim-lua",
-          "hrsh7th/cmp-cmdline",
           "hrsh7th/cmp-buffer",
+          -- Snippet Engine & its associated nvim-cmp source
+          {
+            'L3MON4D3/LuaSnip',
+            build = (function()
+              -- Build Step is needed for regex support in snippets
+              return 'make install_jsregexp'
+            end)(),
+          },
+          'saadparwaiz1/cmp_luasnip',
         },
       }
     },
@@ -34,7 +42,7 @@ return {
     config = function()
       local servers = {
         phpactor = {
-          filetypes = { "php" },
+          -- cmd = { "phpactor", "language-server", "-vvv" }
         },
         bashls = {},
         lua_ls = {
@@ -46,6 +54,7 @@ return {
           },
         },
         jsonls = {},
+        twiggy_language_server = {},
       }
 
       -- Setup neovim lua config
@@ -88,56 +97,66 @@ return {
         nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
       end
 
+      -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+
       -- Setup Mason
       require("mason").setup()
       require("mason-lspconfig").setup({
         ensure_installed = vim.tbl_keys(servers)
       })
-
-      -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
-      --local capabilities = vim.tbl_deep_extend("force", {}, vim.lsp.protocol.make_client_capabilities(),
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
-
-      require("mason-lspconfig").setup_handlers {
-        function(server_name)
-          require("lspconfig")[server_name].setup {
-            capabilities = capabilities,
-            on_attach = on_attach,
-            settings = servers[server_name],
-            filetypes = (servers[server_name] or {}).filetypes
-          }
-        end
+      require("mason-lspconfig").setup {
+        handlers = {
+          function(server_name)
+            local server = servers[server_name] or {}
+            -- This handles overriding only values explicitly passed
+            -- by the server configuration above. Useful when disabling
+            -- certain features of an LSP (for example, turning off formatting for tsserver)
+            server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
+            server.on_attach = on_attach
+            require("lspconfig")[server_name].setup(server)
+          end,
+        },
       }
 
       local cmp = require("cmp")
+      local luasnip = require("luasnip")
+      luasnip.config.setup {}
 
       cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
         completion = {
           completeopt = "menu,menuone,noinsert",
         },
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
+          { name = "luasnip" },
           { name = "nvim-lsp-signature-help" },
           { name = "path" },
           { name = "nvim_lua" },
-          { name = "cmdline" },
         }, {
           { name = "buffer" },
         }),
 
         mapping = cmp.mapping.preset.insert {
-          ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-          ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+          ["<C-n>"] = cmp.mapping.select_next_item(),
+          ["<C-p>"] = cmp.mapping.select_prev_item(),
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
-          ["<Tab>"] = cmp.mapping.confirm({
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
-          }),
+          ["<C-y>"] = cmp.mapping.confirm(),
         },
+
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        }
 
       })
     end
