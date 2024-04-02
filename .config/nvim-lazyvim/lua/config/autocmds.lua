6b22a22635372956f6b6b7ab6468903d445fd987
Cristian Radulescu <cristian67@gmail.com>
2024-04-03 00:57:15 +0300

    nvim - cmp config; set filetype for dotenv files

--- .config/nvim-lazyvim/lua/config/autocmds.lua
+++ .config/nvim-lazyvim/lua/config/autocmds.lua
@@ -1,3 +1,10 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Set filetype for environment specific dotenv files
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  desc = "Set filetype for .env.local",
+  pattern = ".env.*",
+  command = "set filetype=sh",
+})
---
+++ .config/nvim-lazyvim/lua/plugins/coding_cmp.lua
@@ -0,0 +1,21 @@
+return {
+  "hrsh7th/nvim-cmp",
+  opts = function(_, opts)
+    local cmp = require("cmp")
+
+    -- Accept completion with Tab instead of Enter
+    opts.mapping = vim.tbl_extend("force", opts.mapping, {
+      ["<Tab>"] = cmp.mapping.confirm({ select = true }),
+    })
+    opts.mapping["<CR>"] = nil
+
+    opts.experimental = {
+      ghost_text = false,
+    }
+
+    opts.window = {
+      completion = cmp.config.window.bordered(),
+      documentation = cmp.config.window.bordered(),
+    }
+  end,
+}
