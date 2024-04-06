03ad7697151c2808f7c1b7d4256c7258f1533e45
Cristian Radulescu <cristian67@gmail.com>
2024-04-06 15:43:57 +0300

    nvim - telescope improve search in all files from picker

--- .config/nvim-lazyvim/lua/plugins/editor_telescope.lua
+++ .config/nvim-lazyvim/lua/plugins/editor_telescope.lua
@@ -5,10 +5,45 @@ return {
       mappings = {
         i = {
           -- Search in all (hidden and ignored) files
-          ["<a-a>"] = function()
+          ["<a-a>"] = function(prompt_bufnr)
             local action_state = require("telescope.actions.state")
             local line = action_state.get_current_line()
-            LazyVim.telescope("find_files", { hidden = true, no_ignore = true, default_text = line })()
+            local picker = action_state.get_current_picker(prompt_bufnr)
+
+            local new_opts = {}
+            local builtin_action = ""
+
+            -- LazyVim will force "git_files" if it detects a ".git" dir, so usually
+            -- this is the picker that is triggered. When switching to all files I can
+            -- also change the picker.
+            if picker.prompt_title == "Git Files" or picker.prompt_title == "Find Files" then
+              builtin_action = "find_files"
+              new_opts = {
+                hidden = true,
+                no_ignore = true,
+              }
+            elseif picker.prompt_title == "Live Grep" then
+              builtin_action = "live_grep"
+              new_opts = {
+                vimgrep_arguments = {
+                  "rg",
+                  "--color=never",
+                  "--no-heading",
+                  "--with-filename",
+                  "--line-number",
+                  "--column",
+                  "--smart-case",
+                  "--no-ignore-vcs",
+                  "--hidden",
+                },
+              }
+            end
+
+            if builtin_action ~= "" then
+              require("telescope.builtin")[builtin_action](vim.tbl_extend("force", new_opts, { default_text = line }))
+            else
+              print("Refresh not implemented for picker " .. picker.prompt_title)
+            end
           end,
         },
       },
