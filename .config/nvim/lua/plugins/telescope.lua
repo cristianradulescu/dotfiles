dedd1ef02447937355988a2d6b07db0ac21538ec
Cristian Radulescu <cristian67@gmail.com>
2024-04-23 18:38:03 +0300

    nvim - telescope toggle all files

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -31,12 +31,65 @@ return {
           },
           mappings = {
             i = {
+              -- TODO: what are these shortcuts?
               ["<C-u>"] = false,
               ["<C-d>"] = false,
+
+              -- Search in all (hidden and ignored) files
+              ["<a-a>"] = function(prompt_bufnr)
+                local action_state = require("telescope.actions.state")
+                local line = action_state.get_current_line()
+                local picker = action_state.get_current_picker(prompt_bufnr)
+                local picker_prompt_title = picker.prompt_title
+
+                local new_opts = {}
+                local new_builtin_action = ""
+
+                local opts_find_files_with_hidden_and_no_ignore = {
+                  hidden = true,
+                  no_ignore = true,
+                }
+                local opts_vimgrep_arguments_with_hidden_and_no_ignore = {
+                  vimgrep_arguments = {
+                    "rg",
+                    "--color=never",
+                    "--no-heading",
+                    "--with-filename",
+                    "--line-number",
+                    "--column",
+                    "--smart-case",
+                    "--no-ignore-vcs",
+                    "--hidden",
+                  },
+                }
+
+                -- By default I will search in not hidden and not ignored files. When switching to all files I can
+                -- also change the picker.
+                if picker_prompt_title == "Git Files" or picker_prompt_title == "Find Files" then
+                  new_builtin_action = "find_files"
+                  new_opts = opts_find_files_with_hidden_and_no_ignore
+                elseif picker_prompt_title == "Live Grep" then
+                  new_builtin_action = "live_grep"
+                  new_opts = opts_vimgrep_arguments_with_hidden_and_no_ignore
+                elseif picker_prompt_title:sub(1, #"Find Word") == "Find Word" then
+                  new_builtin_action = "grep_string"
+                  new_opts = vim.tbl_extend("force", opts_vimgrep_arguments_with_hidden_and_no_ignore, {
+                    word_match = "-w",
+                    -- the prompt title will have the search term, will get it from there instead from the current line
+                    search = picker_prompt_title:match("Find Word %((.*)%)"),
+                  })
+                end
+
+                if new_builtin_action ~= "" then
+                  require("telescope.builtin")[new_builtin_action](
+                    vim.tbl_extend("force", new_opts, { default_text = line })
+                  )
+                else
+                  print("Refresh not implemented for picker " .. picker.prompt_title)
+                end
+              end,
             },
           },
-          -- Search in non-vcs and hidden files (--no-ignore-vcs, --hidden)
-          vimgrep_arguments = { "rg", "--color=never", "--no-heading", "--with-filename", "--line-number", "--column", "--smart-case", "--no-ignore-vcs", "--hidden" }
         }
       })
       pcall(require("telescope").load_extension, "fzf")
--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -96,10 +96,7 @@ vim.keymap.set('n', '<leader>/', function()
 end, { desc = '[/] Fuzzily search in current buffer' })
 
 -- Search in all files including hidden and ignored by Git
-vim.keymap.set('n', '<leader>sf', function()
- require('telescope.builtin').find_files({ hidden = true, no_ignore = true })
-end, { desc = '[S]earch [F]iles' })
-
+vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files , { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sk', require('telescope.builtin').keymaps, { desc = '[S]earch [K]eymaps' })
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
