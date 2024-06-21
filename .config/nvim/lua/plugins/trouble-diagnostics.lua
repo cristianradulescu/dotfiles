f7a3c3a1b0204c6ee8b697ff9325520562cda228
Cristian Radulescu <cristian67@gmail.com>
2024-06-21 16:49:09 +0300

    nvim - fix trouble key mapping

--- .config/nvim/lua/plugins/trouble-diagnostics.lua
+++ .config/nvim/lua/plugins/trouble-diagnostics.lua
@@ -3,7 +3,7 @@ return {
   "folke/trouble.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   opts = {},
-  vim.keymap.set("n", "<leader>q", "<cmd>Trouble diagnostics toggle filter.buf=0 focus=true", { desc = "Open diagnostics" }),
+  vim.keymap.set("n", "<leader>q", "<cmd>Trouble diagnostics toggle filter.buf=0 focus=true<cr>", { desc = "Open diagnostics" }),
   vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Previous diagnostic" }),
   vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Next diagnostic" }),
 }
