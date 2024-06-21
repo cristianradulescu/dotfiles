63dd7ec094d6af325de1bd53c2fb64ca7054df83
Cristian Radulescu <cristian67@gmail.com>
2024-06-21 16:50:01 +0300

    nvim - automatically install configured formatters

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,13 +1,44 @@
 return {
-  -- Code formatting
+  -- Code formattin
   "stevearc/conform.nvim",
+  event = "BufReadPost",
   opts = {},
   config = function()
+    ---Check if the formatter is found for current buffer and install it if missing using Mason.
+    ---Mason may use a different name for formatters, in that case the mason_pkg_name needs to be provided.
+    ---@param formatter_name string
+    ---@param mason_pkg_name? string
+    local function ensure_installed(formatter_name, mason_pkg_name)
+      if not mason_pkg_name then
+        mason_pkg_name = formatter_name
+      end
+
+      if false == require("conform").get_formatter_info(formatter_name).available then
+        vim.cmd("MasonInstall " .. mason_pkg_name)
+      end
+    end
+
     require("conform").setup({
       formatters_by_ft = {
-        sql = { "sqlfmt" },
-        mysql = { "sqlfmt" },
-        lua = { "luaformatter" }
+        lua = function()
+          ensure_installed("stylua")
+          return { "stylua" }
+        end,
+
+        php = function()
+          ensure_installed("php_cs_fixer", "php-cs-fixer")
+          return { "php_cs_fixer" }
+        end,
+
+        sql = function()
+          ensure_installed("sqlfmt")
+          return { "sqlfmt" }
+        end,
+
+        mysql = function()
+          ensure_installed("sqlfmt")
+          return { "sqlfmt" }
+        end,
       },
     })
   end,
@@ -18,7 +49,7 @@ return {
         require("conform").format({ async = true, lsp_fallback = true })
       end,
       mode = "",
-      desc = "Format code (with Conform)"
-    }
-  }
+      desc = "Format code (with Conform)",
+    },
+  },
 }
