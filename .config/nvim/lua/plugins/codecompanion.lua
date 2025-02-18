48a8ff454199647dd2d24dc04d125234dd0e4cd4
Cristian Radulescu <cristian67@gmail.com>
2025-02-18 17:38:59 +0200

    nvim - add AI assistant

---
+++ .config/nvim/lua/plugins/codecompanion.lua
@@ -0,0 +1,22 @@
+return {
+  "olimorris/codecompanion.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  cond = function()
+    return vim.g.copilot_enabled
+  end,
+  config = function()
+    require("codecompanion").setup({
+      strategies = {
+        chat = {
+          adapter = "copilot",
+        },
+        inline = {
+          adapter = "copilot",
+        },
+      },
+    })
+  end,
+}
