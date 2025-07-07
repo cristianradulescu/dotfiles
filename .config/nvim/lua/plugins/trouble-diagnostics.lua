31f2f5f929b857a4e024dc4c7c788529d69867ab
Cristian Radulescu <cristian67@gmail.com>
2025-07-07 22:38:38 +0300

    nvim cleanup + tmux theme

--- .config/nvim/lua/plugins/trouble-diagnostics.lua
+++
@@ -1,7 +0,0 @@
-return {
-  -- Diagnostic messages
-  "folke/trouble.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  opts = {},
-  vim.keymap.set("n", "<leader>q", "<cmd>Trouble diagnostics toggle filter.buf=0 focus=true<cr>", { desc = "Open diagnostics" }),
-}
